b2ebe82bc4e91536bb9eaba77d74bd9635b210b4
Mahmudul Hasan <17101120@uap-bd.edu>
2021-09-30 15:37:59 +0600

    Basic routing Code

--- routing/src/App.js
+++ routing/src/App.js
@@ -1,23 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import HomePage from './components/Homepage/HomePage';
+import Friends from './components/Friends/Friends';
+import Contact from './components/Contact/Contact';
+import About from './components/Homepage/About/About';
+import NotFound from './components/NoteFound/NotFound';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Switch >
+          <Route exact path='/'>
+            <HomePage></HomePage>
+          </Route>
+          <Route path='/home'>
+            <HomePage></HomePage>
+          </Route>
+          <Route path='/friends'>
+            <Friends></Friends>
+
+          </Route>
+          <Route path='/contact'>
+            <Contact></Contact>
+
+          </Route>
+
+          <Route >
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
